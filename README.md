a08a640dc22faada3855fd7b47261079ae82064c
Connor E <38229097+c-edw@users.noreply.github.com>
2018-04-10 23:17:12 +0100

    Enable CLI use.

--- README.md
+++ README.md
@@ -3,6 +3,11 @@
 #### What is it?
 Finch is a CLI tool which allows you to automatically enhance an image library using Google's Vision API, by replacing each image with the highest resolution version found. It is still currently work-in-progress.
 
+#### How can I use it?
+- Clone this repository
+- Run `cargo build --release`
+- Run `finch -h` for a list of parameters
+
 # What needs doing?
 - Cleanup  
 - CLI implementation
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,50 @@
 extern crate base64;
+extern crate clap;
+extern crate glob;
 extern crate image;
 extern crate reqwest;
-#[macro_use]
-extern crate serde_json;
+
 #[macro_use]
 extern crate serde_derive;
-extern crate glob;
+#[macro_use]
+extern crate serde_json;
 
 use reqwest::Client;
-use base64::encode;
 use image::{DynamicImage, GenericImage};
+use base64::encode;
 use glob::glob;
+use clap::{App, Arg};
 
 use std::fs::File;
 use std::io::Read;
-
-const API_KEY: &str = "";
+use std::path::Path;
+use std::env::current_dir;
 
 fn main() {
-    for entry in glob("assets/*").unwrap() {
-        if let Ok(file) = entry {
-            let name = file.to_str().unwrap();
-            process(name);
-            println!("Processed {}.", name);
-        }
+    let matches = App::new("Finch")
+        .version("0.1.0")
+        .about("A tool for enhancing and organising an image collection using Google's Vision API.")
+        .arg(
+            Arg::with_name("key")
+                .short("k")
+                .long("key")
+                .value_name("key")
+                .required(true)
+                .help("Sets the Google Vision API key."),
+        )
+        .get_matches();
+
+    let key = matches.value_of("key").unwrap();
+
+    // TODO: Surely we can do this better.
+    let iter = glob(current_dir().unwrap().join("**/*").to_str().unwrap()).unwrap();
+
+    for entry in iter {
+        process_file(&entry.unwrap(), key);
     }
 }
 
-fn process(file: &str) {
+fn process_file(file: &Path, key: &str) {
     // Create vector to hold image data.
     let mut buf = Vec::new();
 
@@ -41,11 +58,12 @@ fn process(file: &str) {
     let input = image::load_from_memory(&buf).unwrap();
 
     // Grab the highest resolution version of this image.
-    let output = get_highest_res(&buf);
+    let output = get_highest_res(&buf, key);
 
+    // Only bother replacing the file if it's a higher resolution.
     if output.width() > input.width() && output.height() > input.height() {
         image::save_buffer(
-            file,
+            file.with_extension("png"),
             output.raw_pixels().as_slice(),
             output.width(),
             output.height(),
@@ -77,12 +95,15 @@ struct Responses {
 }
 
 /// Use the Google Vision API do a reverse image search, and get the highest resolution version of an image.
-fn get_highest_res(buf: &[u8]) -> DynamicImage {
+fn get_highest_res(buf: &[u8], key: &str) -> DynamicImage {
+    // Encode image buffer as base64.
+    let buf = encode(&buf);
+
     // Construct request body.
     let json = json!({
         "requests": [{
             "image": {
-                "content": encode(&buf)
+                "content": &buf
             },
             "features": [{
                 "type": "WEB_DETECTION"
@@ -94,7 +115,7 @@ fn get_highest_res(buf: &[u8]) -> DynamicImage {
     let mut res = Client::new()
         .post(&format!(
             "https://vision.googleapis.com/v1/images:annotate?key={}",
-            API_KEY
+            key
         ))
         .body(json.to_string())
         .send()
@@ -104,9 +125,7 @@ fn get_highest_res(buf: &[u8]) -> DynamicImage {
     let values = res.json::<Responses>().unwrap();
 
     // Get the URL of the first matching image returned.
-    let url = values.responses[0]
-        .webDetection
-        .fullMatchingImages[0]
+    let url = values.responses[0].webDetection.fullMatchingImages[0]
         .url
         .to_owned();
 
