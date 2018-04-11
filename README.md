9a7f2b195211fc5286e587749f709006378d32ae
Connor E <38229097+c-edw@users.noreply.github.com>
2018-04-11 18:27:26 +0100

    Fix non-files being picked up by pattern.

--- README.md
+++ README.md
@@ -6,9 +6,10 @@ Finch is a CLI tool which allows you to automatically enhance an image library u
 #### How can I use it?
 - Clone this repository
 - Run `cargo build --release`
+- Link or copy the binary to your `bin` directory (optional)
 - Run `finch -h` for a list of parameters
 
 # What needs doing?
 - Cleanup  
-- CLI implementation
+- ~~CLI implementation~~ (needs extending)
 - Publish to `crates.io`
--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,19 @@ extern crate image;
 extern crate reqwest;
 
 #[macro_use]
-extern crate serde_derive;
-#[macro_use]
 extern crate serde_json;
 
 use reqwest::Client;
-use image::{DynamicImage, GenericImage};
 use base64::encode;
 use glob::glob;
 use clap::{App, Arg};
+use serde_json::Value;
+use image::GenericImage;
 
-use std::fs::File;
-use std::io::Read;
+use std::fs::{self, File};
+use std::io::{Error, Read};
 use std::path::Path;
-use std::env::current_dir;
+use std::env;
 
 fn main() {
     let matches = App::new("Finch")
@@ -30,109 +29,90 @@ fn main() {
                 .long("key")
                 .value_name("key")
                 .required(true)
-                .help("Sets the Google Vision API key."),
+                .help("Defines the Google Vision API key."),
         )
         .get_matches();
 
     let key = matches.value_of("key").unwrap();
 
     // TODO: Surely we can do this better.
-    let iter = glob(current_dir().unwrap().join("**/*").to_str().unwrap()).unwrap();
+    let entries = glob(env::current_dir().unwrap().join("**/*.*").to_str().unwrap()).unwrap();
 
-    for entry in iter {
-        process_file(&entry.unwrap(), key);
+    for entry in entries {
+        process_file(&entry.unwrap(), key).unwrap();
     }
 }
 
-fn process_file(file: &Path, key: &str) {
-    // Create vector to hold image data.
+fn process_file(path: &Path, key: &str) -> Result<(), Error> {
+    let mut file = File::open(path).expect("Unable to find input file.");
+
+    // Read the image into the vector.
     let mut buf = Vec::new();
-
-    // Read the image into the vector.
-    File::open(file)
-        .expect("Unable to find input file.")
-        .read_to_end(&mut buf)
-        .unwrap();
-
-    // Read the image into the vector.
-    let input = image::load_from_memory(&buf).unwrap();
+    file.read_to_end(&mut buf)?;
 
     // Grab the highest resolution version of this image.
-    let output = get_highest_res(&buf, key);
+    if let Some(output) = get_highest_res(&buf, key) {
+        // Delete original file.
+        fs::remove_file(path).unwrap();
 
-    // Only bother replacing the file if it's a higher resolution.
-    if output.width() > input.width() && output.height() > input.height() {
+        let image = image::load_from_memory(&output).unwrap();
+
+        // Write out new image as a PNG.
         image::save_buffer(
-            file.with_extension("png"),
-            output.raw_pixels().as_slice(),
-            output.width(),
-            output.height(),
-            output.color(),
+            path.with_extension("png"),
+            &image.raw_pixels(),
+            image.width(),
+            image.height(),
+            image.color(),
         ).unwrap();
     }
-}
 
-#[derive(Deserialize)]
-struct Images {
-    url: String,
-}
-
-#[allow(non_snake_case)]
-#[derive(Deserialize)]
-struct Matching {
-    fullMatchingImages: Vec<Images>,
-}
-
-#[allow(non_snake_case)]
-#[derive(Deserialize)]
-struct Detections {
-    webDetection: Matching,
-}
-
-#[derive(Deserialize)]
-struct Responses {
-    responses: Vec<Detections>,
+    Ok(())
 }
 
 /// Use the Google Vision API do a reverse image search, and get the highest resolution version of an image.
-fn get_highest_res(buf: &[u8], key: &str) -> DynamicImage {
+fn get_highest_res(buf: &[u8], key: &str) -> Option<Vec<u8>> {
+    // Assemble URL with API key.
+    let url = format!(
+        "https://vision.googleapis.com/v1/images:annotate?key={}",
+        key
+    );
+
     // Encode image buffer as base64.
     let buf = encode(&buf);
 
-    // Construct request body.
+    // Assemble request body.
     let json = json!({
-        "requests": [{
-            "image": {
-                "content": &buf
+       "requests": [{
+            "image": { 
+                "content": &buf 
             },
-            "features": [{
-                "type": "WEB_DETECTION"
-            }]
+            "features": [
+                { "type": "WEB_DETECTION" }
+            ]
         }]
     });
 
     // Assemble request and send it.
     let mut res = Client::new()
-        .post(&format!(
-            "https://vision.googleapis.com/v1/images:annotate?key={}",
-            key
-        ))
+        .post(url.as_str())
         .body(json.to_string())
         .send()
         .unwrap();
 
     // Deserialise the JSON into Responses.
-    let values = res.json::<Responses>().unwrap();
+    let values = res.json::<Value>().unwrap();
+    let matching = &values["responses"][0]["webDetection"]["fullMatchingImages"];
 
-    // Get the URL of the first matching image returned.
-    let url = values.responses[0].webDetection.fullMatchingImages[0]
-        .url
-        .to_owned();
+    // Get the URL of the first image returned.
+    if let Some(v) = matching[0]["url"].as_str() {
+        let mut new = reqwest::get(v).unwrap();
 
-    let mut buf = Vec::new();
+        let mut buf = Vec::new();
+        new.copy_to(&mut buf).unwrap();
 
-    let mut result = reqwest::get(&url).unwrap();
-    result.copy_to(&mut buf).unwrap();
-
-    image::load_from_memory(buf.as_slice()).unwrap()
+        Some(buf)
+    } else {
+        None
+    }
 }
